8624fa15d2dd2f9e567223c45f7d16bea16c2f15
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-18 20:26:14 +0700

    pour bastien

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+//>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\\
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+  Image,
+  ScrollView,
+} from "react-native";
+//>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\\
+import Constants from "expo-constants";
+console.log(Constants.statusBarHeight);
+//>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\\
+console.log(Platform.OS);
+
+import colors from "./assets/colors";
+const { grey, greyName, black, lightBlack } = colors;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView>
+        <StatusBar style="dark" backgroundColor="pink" />
+        <ScrollView style={{ backgroundColor: "aqua" }}>
+          <View style={[styles.container]}>
+            <View
+              style={[styles.bgGrey]}
+              marginTop={
+                Platform.OS === "android" ? Constants.statusBarHeight : 0
+              }
+            >
+              <Image
+                style={{
+                  height: 50,
+                  width: 70,
+                  justifyContent: "center",
+                }}
+                resizeMode="contain"
+                source={{
+                  uri: "https://ia.media-imdb.com/images/M/MV5BOTc2MzQ4MjAwM15BMl5BcG5nXkFtZTgwMzY2MzQ2OTE@._V1_.png",
+                }}
+              />
+            </View>
+            <View style={[styles.bgLightBlack, styles.flex3]} />
+            {/* <View style={[styles.bgBlack]}></View> */}
+            <View style={[styles.bgLightBlack, styles.flex4]} />
+
+            {/* <View
+              style={{ height: 50, width: 70, backgroundColor: "red" }}
+            ></View> */}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "aqua",
+  },
+  flex2: {
+    flex: 2,
+  },
+  flex3: {
+    // heightDi"r00%"ection: "column",
+    flex: 3,
+  },
+  flex4: {
+    flex: 4,
+    padding: 5,
+  },
+  height: {
+    height: 200,
+  },
+  bgGrey: {
+    backgroundColor: grey,
+  },
+  bgGreyName: {
+    backgroundColor: greyName,
+  },
+  bgBlack: {
+    backgroundColor: black,
+  },
+  bgLightBlack: {
+    backgroundColor: lightBlack,
   },
 });
---
+++ assets/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  grey: "#393939",
+  greyName: "#2A2A2A",
+  black: "#1A1A1A",
+  lightBlack: "#212121",
+};
+export default colors;
